1ab3ad5da75549dc23151036f26d086880733369
Ganesh <ganialla156@gmail.com>
2025-10-07 22:07:08 +0530

    feat:Add customerException file

---
+++ src/com/trainingMug/foodiecli/exceptions/CustomerExistException.java
@@ -0,0 +1,7 @@
+package com.trainingMug.foodiecli.exceptions;
+
+public class CustomerExistException extends Exception{
+    public CustomerExistException(String msg){
+        super(msg);
+    }
+}
