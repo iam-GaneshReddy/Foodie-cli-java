a55850881d6a4a0c7d0c04ea42ba148ae9a617a6
Ganesh <ganialla156@gmail.com>
2025-10-08 20:31:31 +0530

    feat:Add RestaurantCustomException file

---
+++ src/com/trainingMug/foodiecli/exceptions/RestaurantExistException.java
@@ -0,0 +1,7 @@
+package com.trainingMug.foodiecli.exceptions;
+
+public class RestaurantExistException extends Exception{
+    public RestaurantExistException(String msg){
+        super(msg);
+    }
+}
