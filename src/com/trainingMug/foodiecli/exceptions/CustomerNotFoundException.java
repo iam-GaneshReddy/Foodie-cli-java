223c6c100716aa73da13f1aeb314ca195d868d68
Ganesh <ganialla156@gmail.com>
2025-10-09 12:23:17 +0530

    feat:add CustomerNotFoundException

---
+++ src/com/trainingMug/foodiecli/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.trainingMug.foodiecli.exceptions;
+
+public class CustomerNotFoundException extends Exception{
+    public CustomerNotFoundException(String msg){
+        super(msg);
+    }
+}
