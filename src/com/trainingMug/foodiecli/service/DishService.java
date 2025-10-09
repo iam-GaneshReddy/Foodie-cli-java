cb8cdb362abb4d01fa738d9eff318435b3da7582
Ganesh <ganialla156@gmail.com>
2025-10-09 10:21:40 +0530

    feat:DishService file updated

--- src/com/trainingMug/foodiecli/service/DishService.java
+++ src/com/trainingMug/foodiecli/service/DishService.java
@@ -1,8 +1,15 @@
 package com.trainingMug.foodiecli.service;
 
 import com.trainingMug.foodiecli.exceptions.DishAlreadyExistException;
+import com.trainingMug.foodiecli.exceptions.DishNotFoundException;
 import com.trainingMug.foodiecli.model.Dish;
 
+import java.util.List;
+
 public interface DishService {
+    public List<Dish> getDishList();
     public Dish addDish(Dish dish)throws DishAlreadyExistException;
+    public Dish getByDishId(String id)throws DishNotFoundException;
+    public Dish updateDish(Dish dish)throws DishNotFoundException;
+    public void delete(String id) throws DishNotFoundException;
 }
--- src/com/trainingMug/foodiecli/service/DishServiceImpl.java
+++ src/com/trainingMug/foodiecli/service/DishServiceImpl.java
@@ -1,9 +1,12 @@
 package com.trainingMug.foodiecli.service;
 
 import com.trainingMug.foodiecli.exceptions.DishAlreadyExistException;
+import com.trainingMug.foodiecli.exceptions.DishNotFoundException;
 import com.trainingMug.foodiecli.model.Dish;
 import com.trainingMug.foodiecli.repository.DishRepository;
+import com.trainingMug.foodiecli.util.Factory;
 
+import java.util.List;
 import java.util.Optional;
 
 public class DishServiceImpl implements DishService{
@@ -12,12 +15,41 @@ public class DishServiceImpl implements DishService{
     public DishServiceImpl(DishRepository dishRepository){
         this.dishRepository = dishRepository;
     }
+
+    @Override
+    public List<Dish> getDishList() {
+        return dishRepository.getAllDishes();
+    }
+
     @Override
     public Dish addDish(Dish dish) throws DishAlreadyExistException {
         Optional<Dish> optionaDish = dishRepository.getDishBYId(dish.getId());
         if(optionaDish.isPresent())
             throw new DishAlreadyExistException("Dish ALready Found"+dish.getId());
         return this.dishRepository.addDish(dish);
+    }
 
+    @Override
+    public Dish getByDishId(String id) throws DishNotFoundException {
+        Optional<Dish>  optionalDish= dishRepository.getDishBYId(id);
+        if(!optionalDish.isPresent())
+            throw new DishNotFoundException("Dish not found with "+id);
+        return optionalDish.get();
+    }
+
+    @Override
+    public Dish updateDish(Dish dish) throws DishNotFoundException {
+        Optional<Dish> dishById = this.dishRepository.getDishBYId((dish.getId()));
+        if(dishById.isEmpty())
+            throw new DishNotFoundException("Dish Not Found with Id : " + dish.getId());
+        return this.dishRepository.updateDish(dish);
+    }
+
+    @Override
+    public void delete(String id) throws DishNotFoundException{
+        Optional<Dish> dishById = this.dishRepository.getDishBYId(id);
+        if(dishById.isEmpty())
+            throw new DishNotFoundException("Dish Not Found with Id : " + id);
+        this.dishRepository.deleteDish(dishById.get());
     }
 }
