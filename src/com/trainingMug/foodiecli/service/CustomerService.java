7e6c8b82f65e4e3e29cf5b9c0cff38a097db89ad
Ganesh <ganialla156@gmail.com>
2025-10-09 12:22:33 +0530

    feat:add CRUD operations

--- src/com/trainingMug/foodiecli/service/CustomerService.java
+++ src/com/trainingMug/foodiecli/service/CustomerService.java
@@ -1,8 +1,18 @@
 package com.trainingMug.foodiecli.service;
 
 import com.trainingMug.foodiecli.exceptions.CustomerExistException;
+import com.trainingMug.foodiecli.exceptions.CustomerNotFoundException;
 import com.trainingMug.foodiecli.model.Customer;
+
+import java.util.List;
 
 public interface CustomerService {
     public Customer save(Customer customer) throws CustomerExistException;
+
+    public Customer validateEmailPassword(String email,String password)throws CustomerNotFoundException;
+
+    public Customer getById(String id)throws CustomerNotFoundException;
+    public Customer updateCustomer(Customer customer)throws CustomerNotFoundException;
+    public List<Customer> getAllCustomers();
+    public void deleteCustomer(String id) throws CustomerNotFoundException;
 }
--- src/com/trainingMug/foodiecli/service/CustomerServiceImpl.java
+++ src/com/trainingMug/foodiecli/service/CustomerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.trainingMug.foodiecli.service;
 
 import com.trainingMug.foodiecli.exceptions.CustomerExistException;
+import com.trainingMug.foodiecli.exceptions.CustomerNotFoundException;
 import com.trainingMug.foodiecli.model.Customer;
 import com.trainingMug.foodiecli.repository.CustomerRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class CustomerServiceImpl implements CustomerService{
@@ -19,4 +21,40 @@ public class CustomerServiceImpl implements CustomerService{
             throw new CustomerExistException("Customer is already existed"+customer.getCustomerId());
         return this.customerRepository.save(customer);
     }
+    @Override
+    public Customer validateEmailPassword(String email, String password) throws CustomerNotFoundException {
+        Optional<Customer> optionalCustomer=customerRepository.validateEmailPassword(email,password);
+        if(optionalCustomer.isEmpty())
+            throw new CustomerNotFoundException("customer not found please login");
+        return optionalCustomer.get();
+    }
+    @Override
+    public Customer getById(String id) throws CustomerNotFoundException {
+        Optional<Customer> optionalCustomer = customerRepository.findCustomerById(id);
+        if(optionalCustomer.isEmpty())
+            throw new CustomerNotFoundException("customer not found with Id-"+id);
+        return optionalCustomer.get();
+    }
+    @Override
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException {
+        Optional<Customer> customerOptional=customerRepository.findCustomerById(customer.getCustomerId());
+        if(customerOptional.isEmpty())
+            throw new CustomerNotFoundException("please enter the id of Existing customer");
+        return customerRepository.updateCustomer(customer);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return this.customerRepository.getAllCustomers();
+    }
+
+    @Override
+    public void deleteCustomer(String id) throws CustomerNotFoundException {
+        Optional<Customer> optional = customerRepository.findCustomerById(id);
+        if(optional.isEmpty())
+            throw new CustomerNotFoundException("Customer not found pls enter correct details");
+        else
+            customerRepository.deleteCustomer(optional.get());
+    }
+
 }
