4ffe41d903a53f77b5d6dc75b33d6b9baf7fef18
Ganesh <ganialla156@gmail.com>
2025-10-09 12:25:55 +0530

    feat:add CustomerController

--- src/com/trainingMug/foodiecli/controller/CustomerController.java
+++ src/com/trainingMug/foodiecli/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.trainingMug.foodiecli.controller;
 
 import com.trainingMug.foodiecli.exceptions.CustomerExistException;
+import com.trainingMug.foodiecli.exceptions.CustomerNotFoundException;
 import com.trainingMug.foodiecli.model.Customer;
 import com.trainingMug.foodiecli.repository.CustomerRepository;
 import com.trainingMug.foodiecli.service.CustomerServiceImpl;
+
+import java.util.List;
 
 public class CustomerController {
     private CustomerServiceImpl customerService;
@@ -11,8 +14,23 @@ public class CustomerController {
     public CustomerController(CustomerServiceImpl customerService){
         this.customerService=customerService;
     }
-
     public Customer save(Customer customer) throws CustomerExistException {
        return this.customerService.save(customer);
     }
+    public Customer validateCustomerLogin(String email,String password)throws CustomerNotFoundException{
+        return this.customerService.validateEmailPassword(email,password);
+    }
+    public Customer getCustomerById(String id) throws CustomerNotFoundException {
+        return this.customerService.getById(id);
+    }
+    public List<Customer> getAllCustomers(){
+        return this.customerService.getAllCustomers();
+    }
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException {
+        return customerService.updateCustomer(customer);
+    }
+    public void deleteCustomer(String id) throws CustomerNotFoundException {
+        customerService.deleteCustomer(id);
+    }
+
 }
