cc53404b6b0a93ace3e63abe634579c7fbeaaaa5
Ganesh <ganialla156@gmail.com>
2025-10-09 15:24:18 +0530

    chore:Factory file updated

--- src/com/trainingMug/foodiecli/util/Factory.java
+++ src/com/trainingMug/foodiecli/util/Factory.java
@@ -1,10 +1,21 @@
 package com.trainingMug.foodiecli.util;
 
 import com.trainingMug.foodiecli.controller.CustomerController;
+import com.trainingMug.foodiecli.controller.DishController;
+import com.trainingMug.foodiecli.controller.RestaurantController;
+import com.trainingMug.foodiecli.model.Restaurant;
 import com.trainingMug.foodiecli.repository.CustomerRepository;
+import com.trainingMug.foodiecli.repository.DishRepository;
+import com.trainingMug.foodiecli.repository.RestaurantRepository;
 import com.trainingMug.foodiecli.service.CustomerServiceImpl;
+import com.trainingMug.foodiecli.service.DishServiceImpl;
+import com.trainingMug.foodiecli.service.RestaurantServiceImpl;
 
 public class Factory {
+    public static CsvReader getCsvReader(){
+        return new CsvReader();
+    }
+    //Customers class
     public static CustomerRepository getCustomerRepository(){
         return new CustomerRepository();
     }
@@ -14,4 +25,27 @@ public class Factory {
     public static CustomerController getCustomerController(){
         return new CustomerController(getCustomerService());
     }
+
+    //Dishes class
+    public static DishRepository getDishRepository(){
+        return new DishRepository();
+    }
+    public static DishServiceImpl getDishServiceImpl(){
+        return new DishServiceImpl(getDishRepository());
+    }
+    public static DishController getDishController(){
+        return new DishController(getDishServiceImpl());
+    }
+
+    //Restaurant class
+    public static RestaurantRepository getRestaurantRepository(){
+        return new RestaurantRepository();
+    }
+    public static RestaurantServiceImpl getRestaurantService(){
+        return new RestaurantServiceImpl(getRestaurantRepository());
+    }
+    public static RestaurantController getRestaurantController(){
+        return new RestaurantController(getRestaurantService());
+    }
+
 }
