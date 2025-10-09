5bba8259624d900d9dea961818cf64f02ff9e4f6
Ganesh <ganialla156@gmail.com>
2025-10-09 15:25:28 +0530

    feat:add main file

---
+++ src/com/trainingMug/foodiecli/Main.java
@@ -0,0 +1,11 @@
+package com.trainingMug.foodiecli;
+
+
+import com.trainingMug.foodiecli.ui.Menu;
+
+public class Main {
+    public static void main(String[] args)  {
+     Menu menu = new Menu();
+     menu.displayMenu();
+    }
+}
