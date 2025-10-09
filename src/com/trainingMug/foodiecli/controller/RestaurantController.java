9b729c28e132359c71504e54865b49a6d6513753
Ganesh <ganialla156@gmail.com>
2025-10-09 15:22:55 +0530

    chore:RestaurantController updated

--- src/com/trainingMug/foodiecli/controller/RestaurantController.java
+++ src/com/trainingMug/foodiecli/controller/RestaurantController.java
@@ -1,8 +1,13 @@
 package com.trainingMug.foodiecli.controller;
 
+import com.trainingMug.foodiecli.exceptions.DishNotFoundException;
 import com.trainingMug.foodiecli.exceptions.RestaurantExistException;
+import com.trainingMug.foodiecli.exceptions.RestaurantNotFoundException;
+import com.trainingMug.foodiecli.model.Dish;
 import com.trainingMug.foodiecli.model.Restaurant;
 import com.trainingMug.foodiecli.service.RestaurantServiceImpl;
+
+import java.util.List;
 
 public class RestaurantController {
     private RestaurantServiceImpl restaurantService;
@@ -12,5 +17,23 @@ public class RestaurantController {
     public Restaurant addRestaurant(Restaurant restaurant) throws RestaurantExistException {
         restaurantService.addRestaurant(restaurant);
         return restaurant;
+    }
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException {
+        return this.restaurantService.searchRestaurant(id);
+    }
+    public List<Restaurant> getAllRestaurants(){
+        return restaurantService.getRestaurantList();
+    }
+    public Restaurant updateRestaurant(Restaurant toBeUpdated) throws RestaurantNotFoundException {
+        return restaurantService.updateRestaurant(toBeUpdated);
+    }
+    public Restaurant searchRestaurant(String id) throws RestaurantNotFoundException {
+        return restaurantService.searchRestaurant(id);
+    }
+    public void deleteRestaurant(String id) throws RestaurantNotFoundException {
+        restaurantService.deleteRestaurant(id);
+    }
+    public List<Dish> getDishItems(String id) throws RestaurantNotFoundException, DishNotFoundException {
+        return this.restaurantService.getDishItems(id);
     }
 }
